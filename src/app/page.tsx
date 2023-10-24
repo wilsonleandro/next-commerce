ec2d737217cc14039452a5b1e3f8e0fb427da3e7
Wilson Leandro <wilson.bleandro@gmail.com>
2023-10-24 11:12:39 -0400

    feat component Product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { ProductType } from "@/types/ProductType"
+import Product from "./components/Product"
 
 async function getProducts() {
   const res = await fetch('https://fakestoreapi.com/products')
@@ -16,7 +17,7 @@ export default async function Home() {
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'>
         
         {products.map((product: ProductType) => (
-          <div key={product.id}>{product.title}</div>
+          <Product key={product.id} product={product} />
         ))}
 
       </div>
