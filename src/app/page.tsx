4fb4cc0ede62ed1f5049d30aa6aa59f371e294b0
Wilson Leandro <wilson.bleandro@gmail.com>
2023-10-24 10:56:51 -0400

    refactor page lista de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,24 @@
-import Image from 'next/image'
+import { ProductType } from "@/types/ProductType"
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json()
+}
+
+export default async function Home() {
+  const products = await getProducts()
+
   return (
     <div className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'>
-        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
-          Prod 1
-        </div>
-        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
-          Prod 2
-        </div>
-        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
-          Prod 3
-        </div>
-        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
-          Prod 4
-        </div>
+        
+        {products.map((product: ProductType) => (
+          <div key={product.id}>{product.title}</div>
+        ))}
+
       </div>
     </div>
   )
