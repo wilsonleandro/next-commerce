b53908a1c7333b044fd4042b86fe4fc72d020477
Wilson Leandro <wilson.bleandro@gmail.com>
2023-10-24 10:42:51 -0400

    wipe page lista de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,21 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Next E-Commerce</h1>
+    <div className='max-w-7xl mx-auto pt-8 px-8 xl:px-0'>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 1
+        </div>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 2
+        </div>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 3
+        </div>
+        <div className='bg-white rounded-lg shadow-lg overflow-hidden'>
+          Prod 4
+        </div>
+      </div>
     </div>
   )
 }
