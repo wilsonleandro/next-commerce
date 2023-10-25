bf9fb392a0bdaa313cf2faf39d3beceafb99095e
Wilson Leandro <wilson.bleandro@gmail.com>
2023-10-25 17:54:57 -0400

    wipe ProductPage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from './components/Navbar'
 import clsx from 'clsx'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ptBR } from '@clerk/localizations'
+import Hydrate from './components/Hydrate'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -22,10 +23,12 @@ export default function RootLayout({
     <ClerkProvider localization={ptBR}>
       <html lang="en">
         <body className={ clsx(inter.className, 'bg-slate-700')}>
-          <Navbar />
-          <main className='h-screen p-16'>
-            {children}
-          </main>
+          <Hydrate>
+            <Navbar />
+            <main className='h-screen p-16'>
+              {children}
+            </main>
+          </Hydrate>
         </body>
       </html>
     </ClerkProvider>
