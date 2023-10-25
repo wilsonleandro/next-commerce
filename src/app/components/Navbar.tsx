83a7faf73153fe73d71df89e399c8cf829b4c456
Wilson Leandro <wilson.bleandro@gmail.com>
2023-10-25 14:26:27 -0400

    feat component Cart

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,36 +1,16 @@
-import { useCartStore } from "@/store";
 import { SignInButton, UserButton } from "@clerk/nextjs";
 import { SignedIn, SignedOut } from "@clerk/nextjs/app-beta";
 import Link from "next/link";
+import Cart from "./Cart";
 
 export default function Navbar() {
-  // const useStore = useCartStore()
-
   return (
     <nav className='fixed top-0 w-full flex items-center py-2 px-8 justify-between z-50 bg-slate-800 text-gray-300'>
       <Link className='uppercase font-bold text-md h-12 flex items-center' href='/'>
         Next Store
       </Link>
       <div className="flex items-center gap-8">
-        <div className="flex items-center cursor-pointer relative">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 0 100 4 2 2"
-            />
-          </svg>
-          <span
-            className="bg-teal-600 text-sm font-bold rounded-full h-5 w-5 flex items-center justify-center absolute left-3 bottom-3"
-          >2</span>
-        </div>
+        <Cart />
         <div>
           <SignedIn>
             <UserButton />
